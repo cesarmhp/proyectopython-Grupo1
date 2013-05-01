58d6feb058b7121d329c5d893b0254654b678c3e
lalisimo <eduardo_santibanez1995@hotmail.com>
2013-05-01 15:48:57 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,5 +1,4 @@
 #Proyecto final
-#===========================================EMPIEZA PARTE DE LECTURA DEL ARCHIVO Y CONVERTIRLO A DICCIONARIO=========
 
 try:
     productos = []
@@ -7,7 +6,7 @@ try:
     inventario = open("inventario.txt","r")
     productos = inventario.readlines()
     inventario.close
-    inventario = []
+    inventario = {}
 except IOError:
     print ("El archivo no se encuentra")
 
@@ -27,7 +26,7 @@ numproducto[len(numproducto)-1] = int(numproducto[len(numproducto)-1])
 
 #Se elimina el número de productos de la lista original quedando sólo el nombre
 for i in range(len(productos)):
-    productos[i] = productos[i][0]  
+    productos[i] = productos[i][0]
 
 #Se integran las dos listas (productos y numproducto) en un diccionario llamado inventario.
 for i in range(len(productos)):
@@ -40,13 +39,13 @@ def login():
     acceso = 0
     intento = 0
     while acceso == 0:
-        archLeer = open("usuariocontraseña.txt","r")#Abro el archivo de texto            #Comienza editado de Juan Mireles
-        articulos = (archLeer.readlines())#Guardo su contenido en una variable           #El archivo en el cual se guarde el usuario y contraseña
-        archLeer.close()#Cierro el archivo                                               #Debe de llamarse "usuariocontraseña" y solo puede contener como maximo
-        articulos2 = []#Creo variable en blanco                                          #2 nombres de usuario con su respectiva contraseña 
-        for i in range(len(articulos)):#Elimino los \n que me apareceran                 # Y tambien la contraseña debe de estar separada por un espacio despues del usuario
-            articulos2.append(articulos[i][0:len(articulos[i])-1])                       #en la misma linea que esta. Ejem: Usuario contraseña
-        palabras = ""                                                                    #                                 Usuario2 contraseña2
+        archLeer = open("usuariocontraseña.txt","r")#Abro el archivo de texto #Comienza editado de Juan Mireles
+        articulos = (archLeer.readlines())#Guardo su contenido en una variable #El archivo en el cual se guarde el usuario y contraseña
+        archLeer.close()#Cierro el archivo #Debe de llamarse "usuariocontraseña" y solo puede contener como maximo
+        articulos2 = []#Creo variable en blanco #2 nombres de usuario con su respectiva contraseña
+        for i in range(len(articulos)):#Elimino los \n que me apareceran # Y tambien la contraseña debe de estar separada por un espacio despues del usuario
+            articulos2.append(articulos[i][0:len(articulos[i])-1]) #en la misma linea que esta. Ejem: Usuario contraseña
+        palabras = "" # Usuario2 contraseña2
         for i in range(len(articulos2)):
             palabras = palabras+articulos[i]
         palabras = palabras.split()
@@ -54,7 +53,7 @@ def login():
         password = input("Password: ")
 
         while intento <= 3:
-            if usuario==palabras[0] and password==palabras[1] or usuario==palabras[2] and password==palabras[3]:   #Termina editado de Juan Mireles.
+            if usuario==palabras[0] and password==palabras[1] or usuario==palabras[2] and password==palabras[3]: #Termina editado de Juan Mireles.
                 print("")
                 return True
             else:
@@ -65,7 +64,7 @@ def login():
             break;
         if intento >= 3:
             print("")
-            print(" *  Las cuentas se han bloqueado porque has intentado mas de 3 veces   *")
+            print(" * Las cuentas se han bloqueado porque has intentado mas de 3 veces *")
             return False
             break;
             
@@ -100,56 +99,54 @@ log = login()
 if log == True:
     print("Bienvenido")
     print()
-    
-while True:
-    opcion=menu()
+    while True:
+        opcion=menu()
 
-    if opcion == 0:
-        print ("Adios!! (:")
-        break
-    if opcion == 1:
-        print(inventario) #imprimi archivo carga el archivo
+        if opcion == 0:
+            print ("Adios!! (:")
+            break
       
-    if opcion == 2:
-            llave = input("¿Qué artículo deseas agregar?: ")
-            valor = int(input("¿Cuántos artículos deseas agregar?: "))
-            llave = llave[0].upper()+ llave[1:].lower()
-            if llave in inventario:
-                for i in range(len(inventario)):
-                    valor = sum(valor,inventario[valor]) #Aquí me falta agregar que se sumen los artículos en caso de que se quieran agregar más del mismo
-            if llave not in inventario:
-                inventario[llave] = valor
+        if opcion == 2:
+                llave = input("¿Qué artículo deseas agregar?: ")
+                valor = int(input("¿Cuántos artículos deseas agregar?: "))
+                llave = llave[0].upper()+ llave[1:].lower()
+                if llave in inventario:
+                    inventario[llave] = inventario[llave]+ valor #Aquí me falta agregar que se sumen los artículos en caso de que se quieran agregar más del mismo
+                if llave not in inventario:
+                    inventario[llave] = valor
                     
-    if opcion == 3:
-        c = input("¿Qué articulo deseas eliminar?: ")
-        c = c[0].upper() + c[1:].lower()
-        try:
-            if c in inventario:
-                print("Actualmente hay", inventario[c], c)
-                x = int(input("¿Cuántos deseas eliminar?: "))
-            print(inventario([cx])) #Aquí me falta hacer que se resten los artículos en caso de que no se eliminen todos
-        except KeyError as c:
-            print("Error", c, "no se encuentra en el inventario")
-        except ValueError:
-            print("Escribe sólo el nombre del artículo")
+        if opcion == 3:
+            c = input("¿Qué articulo deseas eliminar?: ")
+            c = c[0].upper() + c[1:].lower()
+            try:
+                if c in inventario:
+                    print("Actualmente hay", inventario[c], c)
+                    x = int(input("¿Cuántos deseas eliminar?: "))
+                print(inventario([cx])) #Aquí me falta hacer que se resten los artículos en caso de que no se eliminen todos
+            except KeyError as c:
+                print("Error", c, "no se encuentra en el inventario")
+            except ValueError:
+                print("Escribe sólo el nombre del artículo")
             
-    if opcion == 4:
-        try:
-            archiescri = open("inventario.txt","w")
-            for i in lista:
-                archiescri.write(str(i)+"\n")
-            archiescri.close()
-            print ("El inventario se ha guardado en el archivo")
-        except IOError:
-            print("Error. No se puede escribir en el archivo")
-        print("\n")
+        if opcion == 4:
+            try:
+                archiescri = open("inventario.txt","w")
+                for i in lista:
+                    archiescri.write(str(i)+"\n")
+                archiescri.close()
+                print ("El inventario se ha guardado en el archivo")
+            except IOError:
+                print("Error. No se puede escribir en el archivo")
+            print("\n")
                 
-    if opcion == 5:
-        print("===================")
-        print(" ARTICULO - #")
-        print("===================")
-        for i in (inventario):
-            print(i," - ",inventario[i])
+        if opcion == 5:
+            print("\n===================")
+            print(" ARTICULO - #")
+            print("===================")
+            for i in (inventario):
+                print(i," - ",inventario[i])
+
+
 
 
 
