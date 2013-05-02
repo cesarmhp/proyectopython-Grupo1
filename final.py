5e535d11e5e3c44a2007978d5e6b5108430a8b95
aguirrealfonso <a01380774@itesm.mx>
2013-05-02 08:58:15 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,5 +1,10 @@
 #Proyecto final
 
+#======================================COMIENZA FUNCION DE PROCESO DE INICIO DE SESIÓN===========================================
+from login import login            
+#===================================TERMINA FUNCION DE PROCESO DE INICIO DE SESIÓN=================================================
+
+
 try:
     productos = []
     numproducto = []
@@ -34,41 +39,6 @@ for i in range(len(productos)):
 
 #=========================================TERMINA LECTURA DEL ARCHIVO Y CONVERSIÓN A DICCIONARIO======================
 
-#======================================COMIENZA FUNCION DE PROCESO DE INICIO DE SESIÓN===========================================
-def login():
-    acceso = 0
-    intento = 0
-    while acceso == 0:
-        archLeer = open("usuariocontraseña.txt","r")#Abro el archivo de texto #Comienza editado de Juan Mireles
-        articulos = (archLeer.readlines())#Guardo su contenido en una variable #El archivo en el cual se guarde el usuario y contraseña
-        archLeer.close()#Cierro el archivo #Debe de llamarse "usuariocontraseña" y solo puede contener como maximo
-        articulos2 = []#Creo variable en blanco #2 nombres de usuario con su respectiva contraseña
-        for i in range(len(articulos)):#Elimino los \n que me apareceran # Y tambien la contraseña debe de estar separada por un espacio despues del usuario
-            articulos2.append(articulos[i][0:len(articulos[i])-1]) #en la misma linea que esta. Ejem: Usuario contraseña
-        palabras = "" # Usuario2 contraseña2
-        for i in range(len(articulos2)):
-            palabras = palabras+articulos[i]
-        palabras = palabras.split()
-        usuario = input("Usuario: ")
-        password = input("Password: ")
-
-        while intento <= 3:
-            if usuario==palabras[0] and password==palabras[1] or usuario==palabras[2] and password==palabras[3]: #Termina editado de Juan Mireles.
-                print("")
-                return True
-            else:
-                intento = intento + 1
-                print("")
-                print("Datos incorrectos")
-                print("")
-            break;
-        if intento >= 3:
-            print("")
-            print(" * Las cuentas se han bloqueado porque has intentado mas de 3 veces *")
-            return False
-            break;
-            
-#===================================TERMINA FUNCION DE PROCESO DE INICIO DE SESIÓN=================================================
 
 #===============================EMPIEZA FUNCION DEL MENU===============================================================
 def menu (): #Se imprimira un menu con las diferentes opciones que tendra el usuario, de las cuales elegira la que sea mejor para el
